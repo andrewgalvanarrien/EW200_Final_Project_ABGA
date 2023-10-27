0490711b77de9b16ba2092103b6f7aa825cb74db
andrewgalvanarrien <147419694+andrewgalvanarrien@users.noreply.github.com>
2023-10-26 20:51:40 -0400

    more changes

--- game.py
+++ game.py
@@ -1,15 +1,21 @@
 import pygame
 from settings import *
 import sys
+import battleship
+
 pygame.init()
 clock = pygame.time.Clock()
 # score =
-# game_font = pygame.font.Font("assets/fonts/Black_Crayon.ttf", 128)
-
-
-screen = pygame.display.set_mode((SCREEN_WIDTH2, SCREEN_HEIGHT2))
-pygame.display.set_caption("Battle of Surigao Strait")
+game_font = pygame.font.Font("assets/fonts/Wargate-Normal.ttf", 80)
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+pygame.display.set_caption("Torpedo!")
 background = pygame.image.load("assets/images/background.png").convert()
+
+title = game_font.render("Torpedo!", True, (0, 0, 0))
+background.blit(title, (SCREEN_WIDTH - (title.get_width()+ 10), 0))
+
+my_battleship = battleship.Battleship(200,200)
+
 
 while True:
     for event in pygame.event.get():
@@ -17,6 +23,8 @@ while True:
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
+
     screen.blit(background, (0,0))
+    my_battleship.draw(screen)
     pygame.display.flip()
     clock.tick(60)
